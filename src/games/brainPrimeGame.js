b4b155b364abb1ac58026798b6a8748b21dc7ad1
Dmitry <dimetripich@mail.ru>
2023-10-28 21:14:16 +0400

    corrects a calculation error

--- src/games/brainPrimeGame.js
+++ src/games/brainPrimeGame.js
@@ -4,7 +4,9 @@ import startGame from '../index.js';
 
 const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return false;
+    if (num % i === 0) {
+      return false;
+    }
   }
   return num > 1;
 };
